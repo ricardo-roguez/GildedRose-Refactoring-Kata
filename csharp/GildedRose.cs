4dda611c4ce28b5cd4198e5dd9d8db58bbc8bb92
Ricardo Rodriguez <rrodriguez@voxelgroup.net>
2024-03-07 15:33:53 +0100

    wip: step 1

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -5,6 +5,7 @@ namespace csharp
     public class GildedRose
     {
         IList<Item> Items;
+
         public GildedRose(IList<Item> Items)
         {
             this.Items = Items;
@@ -14,21 +15,11 @@ namespace csharp
         {
             for (var i = 0; i < Items.Count; i++)
             {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (Items[i].Quality > 0)
-                    {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            Items[i].Quality = Items[i].Quality - 1;
-                        }
-                    }
-                }
-                else
+                if (Items[i].Name == "Aged Brie")
                 {
                     if (Items[i].Quality < 50)
                     {
-                        Items[i].Quality = Items[i].Quality + 1;
+                        Items[i].Quality += 1;
 
                         if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
                         {
@@ -36,7 +27,7 @@ namespace csharp
                             {
                                 if (Items[i].Quality < 50)
                                 {
-                                    Items[i].Quality = Items[i].Quality + 1;
+                                    Items[i].Quality += 1;
                                 }
                             }
 
@@ -44,21 +35,29 @@ namespace csharp
                             {
                                 if (Items[i].Quality < 50)
                                 {
-                                    Items[i].Quality = Items[i].Quality + 1;
+                                    Items[i].Quality += 1;
                                 }
                             }
                         }
                     }
-                }
 
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    Items[i].SellIn = Items[i].SellIn - 1;
-                }
+                    if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                    {
+                        Items[i].SellIn -= 1;
+                    }
 
-                if (Items[i].SellIn < 0)
+                    if (Items[i].SellIn < 0)
+                    {
+                        if (Items[i].Quality < 50)
+                        {
+                            Items[i].Quality += 1;
+                        }
+                    }
+                }
+                else
                 {
-                    if (Items[i].Name != "Aged Brie")
+
+                    if (true)
                     {
                         if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
                         {
@@ -66,24 +65,125 @@ namespace csharp
                             {
                                 if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
                                 {
-                                    Items[i].Quality = Items[i].Quality - 1;
+                                    Items[i].Quality -= 1;
                                 }
                             }
                         }
                         else
                         {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
+                            if (Items[i].Quality < 50)
+                            {
+                                Items[i].Quality += 1;
+
+                                if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                                {
+                                    if (Items[i].SellIn < 11)
+                                    {
+                                        if (Items[i].Quality < 50)
+                                        {
+                                            Items[i].Quality += 1;
+                                        }
+                                    }
+
+                                    if (Items[i].SellIn < 6)
+                                    {
+                                        if (Items[i].Quality < 50)
+                                        {
+                                            Items[i].Quality += 1;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
+                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                        {
+                            Items[i].SellIn -= 1;
+                        }
+
+                        if (Items[i].SellIn < 0)
+                        {
+                            if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                            {
+                                if (Items[i].Quality > 0)
+                                {
+                                    if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                                    {
+                                        Items[i].Quality -= 1;
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                Items[i].Quality -= Items[i].Quality;
+                            }
                         }
                     }
                     else
                     {
-                        if (Items[i].Quality < 50)
+                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
                         {
-                            Items[i].Quality = Items[i].Quality + 1;
+                            if (Items[i].Quality > 0)
+                            {
+                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                                {
+                                    Items[i].Quality -= 1;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            if (Items[i].Quality < 50)
+                            {
+                                Items[i].Quality += 1;
+
+                                if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                                {
+                                    if (Items[i].SellIn < 11)
+                                    {
+                                        if (Items[i].Quality < 50)
+                                        {
+                                            Items[i].Quality += 1;
+                                        }
+                                    }
+
+                                    if (Items[i].SellIn < 6)
+                                    {
+                                        if (Items[i].Quality < 50)
+                                        {
+                                            Items[i].Quality += 1;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
+                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                        {
+                            Items[i].SellIn -= 1;
+                        }
+
+                        if (Items[i].SellIn < 0)
+                        {
+                            if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                            {
+                                if (Items[i].Quality > 0)
+                                {
+                                    if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                                    {
+                                        Items[i].Quality -= 1;
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                Items[i].Quality -= Items[i].Quality;
+                            }
                         }
                     }
                 }
             }
         }
+        
     }
 }
