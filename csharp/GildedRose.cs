43194b994fa01ca30c57c8d4732f9622a9bfa28c
Ricardo Rodriguez <rrodriguez@voxelgroup.net>
2024-03-09 12:06:40 +0100

    ref: join ifs

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -55,20 +55,14 @@ namespace csharp
                         {
                             Items[i].Quality += 1;
 
-                            if (Items[i].SellIn < 11)
+                            if (Items[i].SellIn < 11 && Items[i].Quality < 50)
                             {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality += 1;
-                                }
+                                Items[i].Quality += 1;
                             }
 
-                            if (Items[i].SellIn < 6)
+                            if (Items[i].SellIn < 6 && Items[i].Quality < 50)
                             {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality += 1;
-                                }
+                                Items[i].Quality += 1;
                             }
                         }
 
