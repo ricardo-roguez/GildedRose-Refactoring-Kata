3cbae22e7a956d7a886017f4fb4c953335188955
Ricardo Rodriguez <rrodriguez@voxelgroup.net>
2024-03-09 12:49:15 +0100

    ref: refactor step 5

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -37,41 +37,25 @@ namespace csharp
                 }
                 else
                 {
-                    if (true)
+                    if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
                     {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                        if (Items[i].Quality < 50)
                         {
-                            if (Items[i].Quality > 0)
+                            Items[i].Quality += 1;
+
+                            if (Items[i].SellIn < 11)
                             {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                                if (Items[i].Quality < 50)
                                 {
-                                    Items[i].Quality -= 1;
+                                    Items[i].Quality += 1;
                                 }
                             }
-                        }
-                        else
-                        {
-                            if (Items[i].Quality < 50)
+
+                            if (Items[i].SellIn < 6)
                             {
-                                Items[i].Quality += 1;
-
-                                if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                                if (Items[i].Quality < 50)
                                 {
-                                    if (Items[i].SellIn < 11)
-                                    {
-                                        if (Items[i].Quality < 50)
-                                        {
-                                            Items[i].Quality += 1;
-                                        }
-                                    }
-
-                                    if (Items[i].SellIn < 6)
-                                    {
-                                        if (Items[i].Quality < 50)
-                                        {
-                                            Items[i].Quality += 1;
-                                        }
-                                    }
+                                    Items[i].Quality += 1;
                                 }
                             }
                         }
@@ -83,23 +67,7 @@ namespace csharp
 
                         if (Items[i].SellIn < 0)
                         {
-                            if (true)
-                            {
-                                if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                                {
-                                    if (Items[i].Quality > 0)
-                                    {
-                                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                                        {
-                                            Items[i].Quality -= 1;
-                                        }
-                                    }
-                                }
-                                else
-                                {
-                                    Items[i].Quality -= Items[i].Quality;
-                                }
-                            }
+                            Items[i].Quality -= Items[i].Quality;
                         }
                     }
                     else
@@ -148,22 +116,19 @@ namespace csharp
 
                         if (Items[i].SellIn < 0)
                         {
-                            if (true)
+                            if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
                             {
-                                if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                                if (Items[i].Quality > 0)
                                 {
-                                    if (Items[i].Quality > 0)
+                                    if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
                                     {
-                                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                                        {
-                                            Items[i].Quality -= 1;
-                                        }
+                                        Items[i].Quality -= 1;
                                     }
                                 }
-                                else
-                                {
-                                    Items[i].Quality -= Items[i].Quality;
-                                }
+                            }
+                            else
+                            {
+                                Items[i].Quality -= Items[i].Quality;
                             }
                         }
                     }
