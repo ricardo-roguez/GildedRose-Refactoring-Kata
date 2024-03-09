99763790cf991a75820550558c45ba4f4a320aaf
Ricardo Rodriguez <rrodriguez@voxelgroup.net>
2024-03-09 12:44:09 +0100

    ref: refactor step 3

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -20,25 +20,6 @@ namespace csharp
                     if (Items[i].Quality < 50)
                     {
                         Items[i].Quality += 1;
-
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].SellIn < 11)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality += 1;
-                                }
-                            }
-
-                            if (Items[i].SellIn < 6)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality += 1;
-                                }
-                            }
-                        }
                     }
 
                     if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
@@ -49,9 +30,9 @@ namespace csharp
                     if (Items[i].SellIn < 0)
                     {
                         if (Items[i].Quality < 50)
-                            {
-                                Items[i].Quality += 1;
-                            }
+                        {
+                            Items[i].Quality += 1;
+                        }
                     }
                 }
                 else
