307975272924228046e21bc0d0e013d3c1a86121
Ricardo Rodriguez <rrodriguez@voxelgroup.net>
2024-03-09 12:34:15 +0100

    final refactor

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -22,25 +22,9 @@ namespace csharp
                         if (Items[i].Quality < 50)
                         {
                             Items[i].Quality += 1;
-
-                            if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                            {
-                                if (Items[i].SellIn < 11 && Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality += 1;
-                                }
-
-                                if (Items[i].SellIn < 6 && Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality += 1;
-                                }
-                            }
                         }
 
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            Items[i].SellIn -= 1;
-                        }
+                        Items[i].SellIn -= 1;
 
                         if (Items[i].SellIn < 0 && Items[i].Quality < 50)
                         {
@@ -66,10 +50,7 @@ namespace csharp
                             }
                         }
 
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            Items[i].SellIn -= 1;
-                        }
+                        Items[i].SellIn -= 1;
 
                         if (Items[i].SellIn < 0)
                         {
@@ -89,12 +70,9 @@ namespace csharp
 
                             Items[i].SellIn -= 1;
 
-                            if (Items[i].SellIn < 0)
+                            if (Items[i].SellIn < 0 && Items[i].Quality > 0)
                             {
-                                if (Items[i].Quality > 0)
-                                {
-                                    Items[i].Quality -= 1;
-                                }
+                                Items[i].Quality -= 1;
                             }
                         }
 
